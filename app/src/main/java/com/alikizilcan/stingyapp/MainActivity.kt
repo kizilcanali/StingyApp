a63c9b0e6075f81474ba1428c9d4f904f1dc3d01
Ali Kızılcan <kizilcanali@gmail.com>
2022-01-11 03:21:53 +0300

    some ui designs

--- app/src/main/java/com/alikizilcan/stingyapp/MainActivity.kt
+++ app/src/main/java/com/alikizilcan/stingyapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.alikizilcan.stingyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.alikizilcan.stingyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostContainer) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
